b2788c3fa10e248ec1ab001675e5de24210ecd72
Hu, Smile <smile.hu@sap.com>
2016-12-22 00:28:34 +0800

    添加爬虫的模板1

---
+++ Scratch/MultithreadingScatch.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import sys
+reload(sys)
+sys.setdefaultencoding = ('utf8')
+
+# from time import sleep, ctime
+# def loop0():
+#     print 'start loop 0 at:', ctime()
+#     sleep(4)
+#     print 'loop 0 done at:', ctime()
+#
+#
+# def loop1():
+#     print 'start loop 1 at:', ctime()
+#     sleep(2)
+#     print 'loop 1 done at:', ctime()
+#
+#
+# def main():
+#     print 'starting at:', ctime()
+#     loop0()
+#     loop1()
+#     print 'all DONE at:', ctime()
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+from time import sleep, ctime
+import thread
+
+
+def loop0():
+    print 'start loop 0 at:', ctime()
+    sleep(4)
+    print 'loop 0 done at:', ctime()
+
+
+def loop1():
+    print 'start loop 1 at:', ctime()
+    sleep(2)
+    print 'loop 1 done at:', ctime()
+
+
+def main():
+    print 'starting at:', ctime()
+    thread.start_new_thread(loop0, ())
+    thread.start_new_thread(loop1, ())
+    sleep(6)
+    print 'all DONE at:', ctime()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Scratch/scratchListTemplate.py
@@ -0,0 +1,48 @@
+# coding:utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+from bs4 import BeautifulSoup
+import requests
+from time import  sleep
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:39.0) Gecko/20100101 Firefox/44.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate',
+    'Connection': 'keep-alive'}
+
+def getListData(outputfile, *args):
+    url = ''.join(args)
+    print url
+    html = requests.get(url, headers=headers).text
+    beautyHtml = BeautifulSoup(html, 'lxml')
+
+    # 死散步：最重要的定制规则
+
+    # 搜索tag
+    tag = beautyHtml.find('div', attrs={'class': 'bottom'})
+    tags = beautyHtml.find_all('div')
+    tags = beautyHtml.find_all(id='html')
+    sub_tags = tag.find_all('div')
+
+    # 获取标签的值
+    content = tag.div.string  # type: bs4.element.NavigableString 也是一种unicode, 可以用str() 进行转换
+    content = tag.head.text  # type: bs4.element.NavigableString 也是一种unicode, 可以用str() 进行转换
+
+    # 获取属性的值
+    property = tag['class'].string
+
+
+if __name__ == '__main':
+    # 第一步：确定好输出文件
+    inputfliePath = '/Users/i309929/Desktop/input.txt'
+    outputfile = open(inputfliePath, 'w')
+
+    # 第二步：定制url
+    url = 'http://www.aqistudy.cn/historydata/index.php'
+    getListData(outputfile, url)
+
+    outputfile.close()
